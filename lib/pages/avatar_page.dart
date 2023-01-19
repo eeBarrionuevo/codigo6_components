026f25135c8a7f8baf4caa34a362800ade9831b9
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2023-01-18 20:45:27 -0500

    FadeInImage

--- lib/pages/avatar_page.dart
+++ lib/pages/avatar_page.dart
@@ -3,6 +3,38 @@ import 'package:flutter/material.dart';
 class AvatarPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Avatar Page"),
+        actions: [
+          Icon(
+            Icons.more_vert,
+          ),
+          CircleAvatar(
+            backgroundColor: Colors.pink,
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          // Image.network(
+          //   "https://images.pexels.com/photos/10162536/pexels-photo-10162536.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+          // ),
+          FadeInImage(
+            placeholder: AssetImage("assets/images/loading.gif"),
+            image: NetworkImage(
+                "https://images.pexels.com/photos/10162536/pexels-photo-10162536.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"),
+          ),
+        ],
+      ),
+      // body: Center(
+      //   child: ElevatedButton(
+      //     child: Text("Atras"),
+      //     onPressed: () {
+      //       Navigator.pop(context);
+      //     },
+      //   ),
+      // ),
+    );
   }
 }

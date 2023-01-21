5e6560f6f0c171d7554b2a68461c71892fe7d3c3
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2023-01-21 13:02:04 -0500

    Mods grid

--- lib/pages/grid_page.dart
+++ lib/pages/grid_page.dart
@@ -22,15 +22,25 @@ class GridPage extends StatelessWidget {
         childAspectRatio: 0.8,
         children: [
           Container(
-            color: Colors.amber,
+            padding: const EdgeInsets.all(12.0),
+            decoration: BoxDecoration(
+              color: const Color(0xffF1F1F1),
+              borderRadius: BorderRadius.circular(14.0),
+              border: Border.all(
+                color: Colors.black.withOpacity(0.12),
+              ),
+            ),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Expanded(
-                  child: Image.network(
-                    "https://www.dc.com/sites/default/files/Char_Gallery_Batman_DTC1018_6053f2162bdf03.97426416.jpg",
-                    fit: BoxFit.cover,
-                    width: double.infinity,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(14.0),
+                    child: Image.network(
+                      "https://www.dc.com/sites/default/files/Char_Gallery_Batman_DTC1018_6053f2162bdf03.97426416.jpg",
+                      fit: BoxFit.cover,
+                      width: double.infinity,
+                    ),
                   ),
                 ),
                 Text(
